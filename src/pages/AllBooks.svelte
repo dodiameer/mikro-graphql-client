a17fc9dfa1be3dbf647424f4788fd5b97c3c4684
dodiameer <mtxshiftg@gmail.com>
2021-02-05 16:07:56 +0300

    refactor: Moved pagination into separate component

--- src/pages/AllBooks.svelte
+++ src/pages/AllBooks.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   /* Imports */
   import { operationStore, query } from "@urql/svelte";
+  import PaginationBar from "../components/AllBooks/PaginationBar.svelte";
   import BookListItem from "../components/BookListItem.svelte";
   import { AllBooksDocument } from "../generated/graphql";
   import pageState from "../stores/pageState";
@@ -15,60 +16,16 @@
   $: $books.variables.offset = $pageState.AllBooks.offset;
 
   query(books);
-
-  /* Helper functions */
-  const nextPage = () => {
-    $pageState.AllBooks.offset += $pageState.AllBooks.limit;
-  };
-
-  const prevPage = () => {
-    $pageState.AllBooks.offset -= $pageState.AllBooks.limit;
-  };
-
-  /* UI reactive variables */
-  $: currentPage = $pageState.AllBooks.offset / $pageState.AllBooks.limit + 1;
-  $: lastPage = () => {
-    try {
-      const division = $books.data.countBooks / $books.variables.limit;
-      return Math.floor(division + 1);
-    } catch {
-      return null;
-    }
-  };
 </script>
 
-<div>
-  <label for="bookLimit">Show in page</label>
-  <select name="limit" id="bookLimit" bind:value="{$pageState.AllBooks.limit}">
-    <option value="{5}">5</option>
-    <option value="{10}">10</option>
-    <option value="{25}">25</option>
-  </select>
-  <button
-    on:click="{prevPage}"
-    disabled="{$books.fetching || $books.variables.offset === 0}">
-    Previous
-  </button>
-  <button
-    on:click="{nextPage}"
-    disabled="{$books.fetching ||
-      $books.data.books.length !== $pageState.AllBooks.limit}">
-    Next
-  </button>
-  <p>
-    Page {currentPage}
-    {#if lastPage() !== null}
-      of {lastPage()}
-    {/if}
-  </p>
-</div>
+<PaginationBar books="{books}" pageState="{pageState}" />
 <ul class="book-list">
   {#if $books.fetching}
     loading...
   {:else if $books.error}
     Error! {$books.error.message}
   {:else}
-    {#key currentPage}
+    {#key $pageState.AllBooks.offset}
       {#each $books.data.books as book, index}
         <BookListItem book="{book}" index="{index}" />
       {:else}
