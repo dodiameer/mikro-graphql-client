6eb80599512d23a6cf1c942561a9e02b8ab92b42
dodiameer <mtxshiftg@gmail.com>
2021-02-05 23:09:08 +0300

    feat: Added author list item component

---
+++ src/components/AllBooks/AuthorListItem.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { cubicOut } from "svelte/easing";
+
+  import { fly } from "svelte/transition";
+
+  import type { AllAuthorsQuery } from "../../generated/graphql";
+
+  export let author: AllAuthorsQuery["authors"][0];
+  export let index: number;
+</script>
+
+<li
+  in:fly="{{
+    delay: index * 100,
+    duration: 300,
+    y: 100,
+    easing: cubicOut,
+  }}">
+  <a href="/author/{author.id}">
+    {author.name}, {author.age ?? "unknown age"}
+  </a>
+</li>
+
+<style>
+  a {
+    color: black;
+  }
+</style>
--- src/pages/AllAuthors.svelte
+++ src/pages/AllAuthors.svelte
@@ -4,6 +4,7 @@
   import type { AllAuthorsQuery } from "../generated/graphql";
   import pageState from "../stores/pageState";
   import PaginationBar from "../components/AllAuthors/PaginationBar.svelte";
+  import AuthorListItem from "../components/AllBooks/AuthorListItem.svelte";
 
   const authors = operationStore(AllAuthorsDocument, {
     limit: $pageState.AllAuthors.limit,
@@ -13,15 +14,19 @@
   $: $authors.variables.offset = $pageState.AllAuthors.offset;
 
   query(authors);
-
-  let data: AllAuthorsQuery["authors"];
-  $: data = $authors?.data?.authors;
 </script>
 
-{#if $authors.fetching}
-  loading...
-{:else if $authors.error}
-  Error! {$authors.error.message}
-{:else}
-  <PaginationBar authors="{authors}" pageState="{pageState}" />
-{/if}
+<PaginationBar authors="{authors}" pageState="{pageState}" />
+<ul>
+  {#if $authors.fetching}
+    loading...
+  {:else if $authors.error}
+    Error! {$authors.error.message}
+  {:else}
+    {#key $pageState.AllAuthors.offset}
+      {#each $authors.data.authors as author, index}
+        <AuthorListItem author="{author}" index="{index}" />
+      {/each}
+    {/key}
+  {/if}
+</ul>
